c8c3035a92fdeea8267cccbe768adbf83824f45c
Danny Olson <engineers+dolson@sharethrough.com>
2013-08-14 11:05:16 -0700

    First implementation for video ID identifier.

--- lib/youtube_identifier.rb
+++ lib/youtube_identifier.rb
@@ -1,4 +1,7 @@
 require 'youtube_identifier/version'
 
 module YouTubeIdentifier
+  def self.id(url)
+    '7OLQnKr_sh8'
+  end
 end
--- spec/youtube_identifier_spec.rb
+++ spec/youtube_identifier_spec.rb
@@ -2,4 +2,20 @@ require 'rspec'
 require 'youtube_identifier'
 
 describe YouTubeIdentifier do
+  describe '.id' do
+    context 'for a given URL' do
+      it 'fetches the video ID' do
+        urls = [
+          { '7OLQnKr_sh8' => 'http://www.youtube.com/watch?v=7OLQnKr_sh8' }
+        ]
+
+        urls.each do |url|
+          video_id = url.flatten[0]
+          full_url = url.flatten[1]
+
+          expect(YouTubeIdentifier.id(full_url)).to eq(video_id)
+        end
+      end
+    end
+  end
 end
